6a9e5178e4db4ce0e62062008221c439501d6e35
imDMK <dominiks8318@gmail.com>
2024-12-27 01:21:53 +0100

    Improve checking spenttime target user.

--- spenttime-plugin/src/main/java/com/github/imdmk/spenttime/litecommands/implementation/SpentTimeCommand.java
+++ spenttime-plugin/src/main/java/com/github/imdmk/spenttime/litecommands/implementation/SpentTimeCommand.java
@@ -8,12 +8,15 @@ import com.github.imdmk.spenttime.user.BukkitPlayerSpentTimeService;
 import com.github.imdmk.spenttime.user.User;
 import com.github.imdmk.spenttime.util.DurationUtil;
 import dev.rollczi.litecommands.annotations.argument.Arg;
+import dev.rollczi.litecommands.annotations.async.Async;
 import dev.rollczi.litecommands.annotations.command.Command;
 import dev.rollczi.litecommands.annotations.context.Context;
 import dev.rollczi.litecommands.annotations.execute.Execute;
 import dev.rollczi.litecommands.annotations.permission.Permission;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.time.Duration;
 
 @Command(name = "spenttime")
 public class SpentTimeCommand {
@@ -41,17 +44,30 @@ public class SpentTimeCommand {
         this.notificationSender.send(player, notification);
     }
 
+    @Async
     @Execute
     @Permission("command.spenttime.target")
     void showTarget(@Context CommandSender sender, @Arg User target) {
-        String userSpentTime = DurationUtil.toHumanReadable(target.getSpentTimeDuration());
+        String targetName = target.getName();
+        String targetSpentTime = DurationUtil.toHumanReadable(this.updateSpentTime(target));
 
         Notification notification = new NotificationFormatter()
                 .notification(this.notificationSettings.targetSpentTime)
-                .placeholder("{PLAYER}", target.getName())
-                .placeholder("{TIME}", userSpentTime)
+                .placeholder("{PLAYER}", targetName)
+                .placeholder("{TIME}", targetSpentTime)
                 .build();
 
         this.notificationSender.send(sender, notification);
+    }
+
+    private Duration updateSpentTime(User target) {
+        Duration playerSpentTime = this.bukkitPlayerSpentTimeService.getSpentTime(target.getUuid());
+        Duration userSpentTime = target.getSpentTimeDuration();
+
+        if (!playerSpentTime.equals(userSpentTime)) {
+            target.setSpentTime(playerSpentTime);
+        }
+
+        return playerSpentTime;
     }
 }
